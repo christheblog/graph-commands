f7461096b29adb576fa5c1d6bb121bacf2fa5263
cacr <christheblog@gmail.com>
2020-04-02 23:48:09 +0100

    Adding clean command

--- command/src/bin/hg-clean.rs
+++ command/src/bin/hg-clean.rs
@@ -1,6 +1,58 @@
-use std::io::Error;
+use clap::{App, Arg};
+use hg_command::utils;
+use hg_command::version;
 
-fn main() -> Result<(), Error> {
-    println!("Cleanup resources from current graph ...");
-    Ok(())
+fn main() {
+    let args = App::new("hg-clean")
+        .version(version::VERSION)
+        .author(version::AUTHOR)
+        .about("Cleans a graph")
+        .arg(
+            Arg::with_name("path")
+                .long("path")
+                .short("p")
+                .help("Use the specified directory instead of the current one")
+                .default_value(".")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("force")
+                .long("force")
+                .short("f")
+                .help("By-pass interactive confirmation")
+                .required(false)
+                .max_values(1)
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("silent")
+                .long("silent")
+                .short("s")
+                .help("Don't print anything on stdout")
+                .required(false)
+                .max_values(1)
+                .takes_value(false),
+        )
+        .get_matches();
+
+    let path = args.value_of("path").unwrap();
+    let force = args.is_present("force");
+    let silent = args.is_present("slient");
+
+    if !force {
+        // FIXME ask for a yes no confirmation
+    }
+
+    if !silent {
+        println!("Cleaning graph under '{}' ...", path);
+    }
+    utils::clean(path).expect(&format![
+        "A problem occured. Path '{}' might not exist, or teh graph is currently lock (check 'lock' file)",
+        path
+    ]);
+
+    if !silent {
+        println!("Done.");
+    }
 }
