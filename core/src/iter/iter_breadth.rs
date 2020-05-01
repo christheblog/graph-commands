d1d0af0e26377b25f68382f5943dee692ea150f6
cacr <christheblog@gmail.com>
2020-05-01 18:36:44 +0100

    Making DFS and BFS iterators producing path

--- core/src/iter/iter_breadth.rs
+++ core/src/iter/iter_breadth.rs
@@ -1,7 +1,8 @@
-///! Graph Iterator implementation
 use crate::directed_graph::DirectedGraph;
 use crate::graph::{Edge, VertexId};
 use crate::iter::iter_datastructure::{Queue, SearchQueue};
+///! Graph Iterator implementation
+use crate::path::Path;
 
 use std::collections::HashSet;
 
@@ -58,6 +59,52 @@ fn empty_bfs_iter(graph: &DirectedGraph) -> BreadthFirstIter {
         graph: graph,
     }
 }
+
+/// Breadth-First Path iterator
+
+pub struct BreadthFirstPathIter<'a> {
+    queue: Queue<Path>,
+    graph: &'a DirectedGraph,
+}
+
+impl<'a> Iterator for BreadthFirstPathIter<'a> {
+    type Item = Path;
+    fn next(&mut self) -> Option<Path> {
+        match self.queue.pop() {
+            None => None,
+            Some(path) => {
+                let vid = path
+                    .last()
+                    .expect("We shouldn't never have any empty path in the queue !");
+                self.graph
+                    .outbound_edges(*vid)
+                    .map(|Edge(_, v)| v)
+                    .for_each(|v| {
+                        if !path.contains_vertex(v) {
+                            self.queue.push(path.append(*v))
+                        }
+                    });
+                Some(path)
+            }
+        }
+    }
+}
+
+/// Returns a new breadth first search iterator on the given graph, starting from the given start_vertex
+pub fn bfs_path_iter_from(graph: &DirectedGraph, start_vertex: VertexId) -> BreadthFirstPathIter {
+    let mut iter = empty_bfs_path_iter(graph);
+    iter.queue.push(Path::from(&vec![start_vertex]));
+    iter
+}
+
+/// Builds an empty iterator from a given graph.
+fn empty_bfs_path_iter(graph: &DirectedGraph) -> BreadthFirstPathIter {
+    BreadthFirstPathIter {
+        queue: Queue::<Path>::new(),
+        graph: graph,
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -154,4 +201,52 @@ mod tests {
             "BFS returned an invalid length"
         ];
     }
+
+    // BFS path
+
+    #[test]
+    fn bfs_path_iterator_return_path_for_all_reachable_nodes_in_a_breadth_first_search_order() {
+        fn edge_from(src: u64, end: u64) -> Edge {
+            Edge(VertexId(src), VertexId(end))
+        }
+
+        fn path_of(v: Vec<u64>) -> Path {
+            Path::from(&v.iter().map(|id| VertexId(*id)).collect())
+        }
+
+
+        let mut g = DirectedGraph::new();
+        g.add_edge(edge_from(1, 2));
+        g.add_edge(edge_from(1, 4));
+        g.add_edge(edge_from(2, 3));
+        g.add_edge(edge_from(2, 5));
+        g.add_edge(edge_from(1, 5));
+        g.add_edge(edge_from(4, 5));
+        g.add_edge(edge_from(4, 6));
+        g.add_edge(edge_from(6, 7));
+        g.add_edge(edge_from(7, 2));
+        // 8 is NOT reachable from 1
+        g.add_edge(edge_from(8, 2));
+
+        let it = bfs_path_iter_from(&g, VertexId(1));
+        assert_eq![
+            it.collect::<Vec<Path>>(),
+            vec!(
+                path_of(vec![1]),
+                path_of(vec![1, 2]),
+                path_of(vec![1, 4]),
+                path_of(vec![1, 5]),
+                path_of(vec![1, 2, 3]),
+                path_of(vec![1, 2, 5]),
+                path_of(vec![1, 4, 5]),
+                path_of(vec![1, 4, 6]),
+                path_of(vec![1, 4, 6, 7]),
+                path_of(vec![1, 4, 6, 7, 2]),
+                path_of(vec![1, 4, 6, 7, 2, 3]),
+                path_of(vec![1, 4, 6, 7, 2, 5]),
+            ),
+            "BFS order is wrong when starting from Vertex 1"
+        ];
+    }
+
 }
--- core/src/iter/iter_depth.rs
+++ core/src/iter/iter_depth.rs
@@ -1,12 +1,13 @@
-///! Graph Iterator implementation
 use crate::directed_graph::DirectedGraph;
 use crate::graph::{Edge, VertexId};
 use crate::iter::iter_datastructure::{SearchQueue, Stack};
+///! Graph Iterator implementation
+use crate::path::Path;
 
 use std::collections::HashSet;
 
-///! Depth-First search iterator
-
+/// Depth-First search iterator
+/// Generates vertex as visited by a depth-first search
 pub struct DepthFirstIter<'a> {
     stack: Stack<VertexId>,
     visited: HashSet<VertexId>,
@@ -54,11 +55,60 @@ pub fn dfs_iter_from(graph: &DirectedGraph, start_vertex: VertexId) -> DepthFirs
     dfs_iter
 }
 
-/// Builds an empty iterator from a given graph.
+/// Builds an empty iterator for a given graph.
 fn empty_dfs_iter(graph: &DirectedGraph) -> DepthFirstIter {
     DepthFirstIter {
         stack: Stack::<VertexId>::new(),
         visited: HashSet::new(),
+        graph: graph,
+    }
+}
+
+/// Depth-First search iterator, Returning a full path from the first vertex
+/// Useful to generate all possible path without cycle from a given vertex
+pub struct DepthFirstPathIter<'a> {
+    stack: Stack<Path>,
+    graph: &'a DirectedGraph,
+}
+
+impl<'a> Iterator for DepthFirstPathIter<'a> {
+    type Item = Path;
+
+    // Note:
+    // children are pushed in reverse order in the stack
+    // First child to be considered by the DFS will be the last one
+    fn next(&mut self) -> Option<Path> {
+        match self.stack.pop() {
+            None => None,
+            Some(path) => {
+                let vid = path
+                    .last()
+                    .expect("We shouldn't never have any empty path in the stack !");
+                self.graph
+                    .outbound_edges(*vid)
+                    .map(|Edge(_, v)| v)
+                    .for_each(|v| {
+                        if !path.contains_vertex(v) {
+                            self.stack.push(path.append(*v));
+                        }
+                    });
+                Some(path)
+            }
+        }
+    }
+}
+
+/// Returns a new depth first search iterator on the given graph, starting from the given start_vertex
+pub fn dfs_iter_path_from(graph: &DirectedGraph, start_vertex: VertexId) -> DepthFirstPathIter {
+    let mut dfs_iter = empty_dfs_path_iter(graph);
+    dfs_iter.stack.push(Path::from(&vec![start_vertex]));
+    dfs_iter
+}
+
+/// Builds an empty path iterator for a given graph.
+fn empty_dfs_path_iter(graph: &DirectedGraph) -> DepthFirstPathIter {
+    DepthFirstPathIter {
+        stack: Stack::<Path>::new(),
         graph: graph,
     }
 }
@@ -159,6 +209,55 @@ mod tests {
             it.collect::<Vec<VertexId>>().len(),
             5,
             "DFS returned an invalid length"
+        ];
+    }
+
+    // DFS path
+
+    #[test]
+    fn dfs_path_iterator_return_path_for_all_reachable_nodes_in_a_depth_first_search_order() {
+        fn edge_from(src: u64, end: u64) -> Edge {
+            Edge(VertexId(src), VertexId(end))
+        }
+
+        fn path_of(v: Vec<u64>) -> Path {
+            Path::from(&v.iter().map(|id| VertexId(*id)).collect())
+        }
+
+        let mut g = DirectedGraph::new();
+        g.add_edge(edge_from(1, 2));
+        g.add_edge(edge_from(1, 4));
+        g.add_edge(edge_from(2, 3));
+        g.add_edge(edge_from(2, 5));
+        g.add_edge(edge_from(1, 5));
+        g.add_edge(edge_from(4, 5));
+        g.add_edge(edge_from(4, 6));
+        g.add_edge(edge_from(6, 7));
+        g.add_edge(edge_from(7, 2));
+        // 8 is NOT reachable from 1
+        g.add_edge(edge_from(8, 2));
+
+        let it = dfs_iter_path_from(&g, VertexId(1));
+        assert_eq![
+            it.collect::<Vec<Path>>(),
+            vec!(
+                path_of(vec![1]),
+                // Last child from Vertex 1
+                path_of(vec![1, 5]),
+                // Middle child
+                path_of(vec![1, 4]),
+                path_of(vec![1, 4, 6]),
+                path_of(vec![1, 4, 6, 7]),
+                path_of(vec![1, 4, 6, 7, 2]),
+                path_of(vec![1, 4, 6, 7, 2, 5]),
+                path_of(vec![1, 4, 6, 7, 2, 3]),
+                path_of(vec![1, 4, 5]),
+                // First child
+                path_of(vec![1, 2]),
+                path_of(vec![1, 2, 5]),
+                path_of(vec![1, 2, 3])
+            ),
+            "DFS order is wrong when starting from Vertex 1"
         ];
     }
 }
